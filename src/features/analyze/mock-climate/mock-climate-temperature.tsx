6f41227b32af72e597c321aa56a605e3d8710c05
Maksim Barsukov <barsukov@bpcbt.com>
2019-11-02 13:57:07 +0300

    MockClimateTemperature add functionality to move data one year back

--- src/features/analyze/mock-climate/mock-climate-temperature.tsx
+++ src/features/analyze/mock-climate/mock-climate-temperature.tsx
@@ -4,15 +4,15 @@ import React from "react";
 export class MockClimateTemperature {
   private data: Record<number, number[]>;
 
-  constructor(private selectedMonths: number[])
+  constructor(private selectedMonths: number[], moved = false)
   {
-    this.data = this.getData();
+    this.data = this.getData(moved);
   }
 
   getGraphData(): { data: number; year: number }[]
   {
     const average = this.getAverage(this.selectedMonths);
-    return average.flatMap(a => Object.entries(a).map(([k,v]) => ({year: +k, data: v})))
+    return average.flatMap(a => Object.entries(a).map(([k, v]) => ({year: +k, data: v})))
   }
 
   getAverage(months: number[]): { [p: string]: number }[]
@@ -27,9 +27,11 @@ export class MockClimateTemperature {
     return sum / values.length;
   }
 
-  private getData()
+  private getData(moved: boolean)
   {
-    return {
+    const data = {
+      1999: [0.3, 2.1, 5.4, 11.6, 13.7, 22.1, 25.5, 23.9, 17.2, 11.3, 0.9, 2.5],
+      2000: [-1.6, 1.5, 3.6, 14.2, 14.2, 20.3, 25.7, 23.7, 16.7, 8.8, 2.9, 0.2],
       2001: [-0.8, 0.4, 6.4, 11.6, 15.1, 19.9, 25.5, 24.4, 18.2, 9.7, 5.8, -2],
       2002: [-2.8, 4.4, 6.9, 9, 15.1, 9.5, 24.9, 21.4, 19.2, 11.6, 6, -7.4],
       2003: [-2.8, 4.4, 6.9, 9, 15.1, 19.5, 24.9, 21.4, 19.2, 11.6, 6, -7.4],
@@ -47,8 +49,23 @@ export class MockClimateTemperature {
       2015: [-1.5, 0.1, 4.3, 9.5, 16.7, 22.5, 24.6, 24.4, 21, 9.4, 5.8, 2.5],
       2016: [-2.4, 3.5, 5.8, 12.7, 16.6, 21.6, 23.6, 25.5, 16.8, 8.5, 3.5, -3.7],
       2017: [-2.4, -2, 5.7, 10.2, 15.6, 20.4, 25.1, 25.5, 20.1, 10.4, 4.7, 3.2],
-      2018: [-1.7, 0.8, 4.2, 11.3, 18.6, 23.3, 26.1, 23.4, 19.2, 12.9, 2.4 , 1]
-    };
+      2018: [-1.7, 0.8, 4.2, 11.3, 18.6, 23.3, 26.1, 23.4, 19.2, 12.9, 2.4, 1]
+    } as Record<number, number[]>;
+
+    return moved ? this.getDataMovedOneYearBack(data) : data;
+  }
+
+  private getDataMovedOneYearBack(data: Record<number, number[]>)
+  {
+    return Object
+      .keys(data)
+      .map(k => ({[(+k + 1).toString()]: data[+k]}))
+      .slice(0, -1)
+      .reduce((acc, c) => {
+        const [k, v]: [string, number[]] = Object.entries(c)[0];
+        acc[+k] = v;
+        return acc;
+      }, {} as Record<number, number[]>);
   }
 }
 

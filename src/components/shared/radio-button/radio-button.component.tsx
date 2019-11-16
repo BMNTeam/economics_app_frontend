be6f750a2d1a8f98136903e4ae637005b6b643e8
Maksim Barsukov <barsukov@bpcbt.com>
2019-11-16 12:37:07 +0300

    Add radio button disabling functionality

--- src/components/shared/radio-button/radio-button.component.tsx
+++ src/components/shared/radio-button/radio-button.component.tsx
@@ -1,8 +1,8 @@
 import React, {ChangeEvent} from "react";
 import {BaseItem} from "../../../models/base-item";
 
-interface RadioButtonProps {
-  option: BaseItem;
+export interface RadioButtonProps {
+  option: BaseItem & { disabled?: boolean;};
   name: string;
   action: (event: ChangeEvent<HTMLInputElement>) => void;
 }
@@ -11,7 +11,9 @@ const RadioButtonComponent: React.FC<RadioButtonProps> = (props) => {
   return (
     <div className="form-check">
       <label className="form-check-label">
-        <input className="form-check-input" type="radio" onChange={props.action} name={props.name} value={props.option.id}/>
+        <input className="form-check-input"
+               disabled={props.option.disabled}
+               type="radio" onChange={props.action} name={props.name} value={props.option.id}/>
         {props.option.name}
         <span className="circle">
                   <span className="check"></span>
